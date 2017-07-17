fd910bfd7a87b80e7d19ee9ab183d2a4630261b7
Linda L Sato <lsato@lib-ads0001.uoregon.edu>
2017-07-17 14:54:56 -0700

    add thisday to config

--- config_example.py
+++ config_example.py
@@ -1,6 +1,15 @@
 # Set to True to use random pages instead of curated selections in PAGES
 RANDOM = True
 
+# Set to True to enable the 'This day in history' function
+THISDAY = False
+
+# Set to earliest year to search with THISDAY
+MINYEAR = 1900
+
+# Set to latest year to search  with THISDAY
+MAXYEAR = 1922
+
 # The number of results that will be displayed
 NUMBER = 4
 
