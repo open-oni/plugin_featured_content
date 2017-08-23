17c9da87d1744e227e928535a5cf5bc2cbe95514
Jeremy Echols <jechols@uoregon.edu>
2017-08-23 14:27:09 -0700

    Fix the API to return select pages, not all

--- views.py
+++ views.py
@@ -6,5 +6,5 @@ from django.shortcuts import render
 from onisite.plugins.featured_content import helpers
 
 def featured(request):
-    all_pages, this_day_title = helpers.get_pages()
+    pages, this_day_title = helpers.get_pages()
     return render(request, 'featured.html', locals())
